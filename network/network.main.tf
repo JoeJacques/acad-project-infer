de18b3793e806c90352bc569f6c455f2fff3136e
Ritesh Ramkhelawon <ritesh.ramkhelawon@outlook.com>
2020-11-24 15:13:24 +0000

    feat: added security group

--- network/network.main.tf
+++ network/network.main.tf
@@ -52,3 +52,20 @@ resource "aws_route_table_association" "route_table_association" {
   subnet_id      = "${element(aws_subnet.public.*.id, count.index)}"
   route_table_id = aws_route_table.route_table.id
 }
+
+# create sec group
+resource "aws_security_group" "sec_group" {
+  vpc_id =aws_vpc.main.id
+  
+  ingress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  tags = {
+    Name = "acad-ritesh-joe-sg"
+    Owner = "acad-ritesh-joe"
+  }
+}
