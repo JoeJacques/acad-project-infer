94c084dda83af69db989ccda1bfb0ddbc585c757
Ritesh Ramkhelawon <ritesh.ramkhelawon@outlook.com>
2020-11-25 10:41:32 +0000

    feat: added security group for fargate

--- modules/network/network.main.tf
+++ modules/network/network.main.tf
@@ -69,3 +69,20 @@ resource "aws_security_group" "sec_group" {
     Owner = "acad-ritesh-joe"
   }
 }
+
+# create sec group
+resource "aws_security_group" "sec_group_fargate" {
+  vpc_id =aws_vpc.main.id
+  
+  ingress {
+    from_port = 0
+    to_port = 3000
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  tags = {
+    Name = "acad-ritesh-joe-sg-fargate"
+    Owner = "acad-ritesh-joe"
+  }
+}
