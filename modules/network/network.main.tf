4261d49bb6b61befb25d97fb0e934e0ab51a8514
JoeJacques <joe.lewis.jacques@gmail.com>
2020-11-25 14:52:40 +0000

    fix: typo

--- modules/network/network.main.tf
+++ modules/network/network.main.tf
@@ -70,28 +70,8 @@ resource "aws_security_group" "sec_group" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-}
-
-  
   tags = {
     Name = "acad-ritesh-joe-sg"
-    Owner = "acad-ritesh-joe"
-  }
-}
-
-# create sec group
-resource "aws_security_group" "sec_group_fargate" {
-  vpc_id = aws_vpc.main.id
-  
-  ingress {
-    from_port = 0
-    to_port = 3000
-    protocol = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  
-  tags = {
-    Name = "acad-ritesh-joe-sg-fargate"
     Owner = "acad-ritesh-joe"
   }
 }
