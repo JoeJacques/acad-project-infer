33ecd4298f0688d0f6a5b9f3f26002afaba32d95
JoeJacques <joe.lewis.jacques@gmail.com>
2020-11-25 10:05:21 +0000

    feat: terraform config

--- main.tf
+++ main.tf
@@ -3,14 +3,14 @@ provider "aws" {
   access_key = var.access_key
   secret_key = var.secret_key
   region = var.region
-  token= var.token
 }
 
 terraform {
   backend "remote" {
-    organization = "acad-project"
+    organization = "terraraform-acad"
+
     workspaces {
-      name = "acad-project-infer"
+      name = "dev"
     }
   }
 }
