dcd7fdbf199a179db955a5352403382d91688797
JoeJacques <joe.lewis.jacques@gmail.com>
2020-11-25 14:47:53 +0000

    fix: security group fix

--- modules/network/network.main.tf
+++ modules/network/network.main.tf
@@ -55,14 +55,23 @@ resource "aws_route_table_association" "route_table_association" {
 
 # create sec group
 resource "aws_security_group" "sec_group" {
-  vpc_id =aws_vpc.main.id
+  vpc_id = aws_vpc.main.id
   
   ingress {
     from_port = 0
     to_port = 0
-    protocol = "-1"
+    protocol = -1
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
   
   tags = {
     Name = "acad-ritesh-joe-sg"
