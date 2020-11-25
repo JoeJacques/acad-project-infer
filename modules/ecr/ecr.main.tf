f0de011620d97ab0cdd448f4bcb79c5fd696aecd
Ritesh Ramkhelawon <ritesh.ramkhelawon@outlook.com>
2020-11-25 16:16:53 +0000

    feat: added ecr creation

--- modules/ecr/ecr.main.tf
+++ modules/ecr/ecr.main.tf
@@ -1,3 +1,4 @@
+# create an ECR repo
 resource "aws_ecr_repository" "ecr_repo" {
   name                 = "acad-joe-ritesh-repo"
   image_tag_mutability = "MUTABLE"
@@ -7,6 +8,40 @@ resource "aws_ecr_repository" "ecr_repo" {
   }
 }
 
-resource "aws_ecs_cluster" "blog-joe-ritesh" {
+# create a cluster
+resource "aws_ecs_cluster" "blog_joe_ritesh" {
   name = "blog-joe-ritesh"
+}
+
+# create a task definition
+resource "aws_ecs_task_definition" "blog_joe_ritesh" {
+  family                = "service"
+  container_definitions = file("./task_definition.json")
+
+  volume {
+    name      = "service-storage"
+    host_path = "/ecs/service-storage"
+  }
+
+  placement_constraints {
+    type       = "memberOf"
+    expression = "attribute:ecs.availability-zone in [eu-west-1]"
+  }
+}
+
+# create a service
+resource "aws_ecs_service" "blog_joe_ritesh" {
+  name = "blog-joe-ritesh"
+  cluster = aws_ecs_cluster.blog_joe_ritesh.arn
+
+  task_definition = aws_ecs_task_definition.blog_joe_ritesh.arn
+  desired_count   = 1
+  # iam_role        = aws_iam_role.foo.arn
+  depends_on      = [aws_ecs_task_definition.blog_joe_ritesh.arn]
+
+  network_configuration {
+    assign_public_ip = true
+    security_groups = aws_security_group.sec_group.id
+    subnets = aws_subnet.public.id
+  }
 }
